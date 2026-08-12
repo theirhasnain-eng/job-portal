00cc99eb571f03f9447a99bed8399642880ef1ee
theirhasnain <theirhasnain@gmail.com>
2026-08-12 00:47:05 -0700

    some changes in frontend url are added

--- frontend/job-portal/src/utils/constant.js
+++ frontend/job-portal/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/api/user"
-export const JOB_API_END_POINT="http://localhost:8000/api/job"
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
-
+export const USER_API_END_POINT = `${BASE_URL}/api/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/job`;
