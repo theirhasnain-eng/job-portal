2cf7af48607bbe56eaf6f9957acf8f0488969659
theirhasnain <theirhasnain@gmail.com>
2026-08-16 22:10:25 -0700

    some files are deleted

--- frontend/job-portal/src/utils/constant.js
+++
@@ -1,5 +0,0 @@
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
-
-export const USER_API_END_POINT = `${BASE_URL}/api/user`;
-export const JOB_API_END_POINT = `${BASE_URL}/api/job`;
-export const Admin_API_END_POINT = `${BASE_URL}/api/admin`;
